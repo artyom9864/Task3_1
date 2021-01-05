80c956e4567588930396c084f1330c441e4e6996
Артём Братчук <artyom9864@mail.ru>
2021-01-05 20:50:33 +0300

    Добавьте файлы проекта.

---
+++ Task3_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Task3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _key = ConsoleKey.Enter;
+            var keyInfo = new ConsoleKeyInfo();
+
+            for (; ; )
+            {
+                do
+                {
+                    string _entryField = Console.ReadLine();
+                    char _firstElement = _entryField.FirstOrDefault();
+                    keyInfo = Console.ReadKey(true);                
+                    Console.WriteLine(_firstElement);                               
+                }
+
+                while (keyInfo.Key != _key);
+                break;
+            }                  
+        }
+    }
+}
+
+
